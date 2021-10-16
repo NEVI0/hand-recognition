f10203813ad8f8dd0d0c40f3c5dd3baa97cceb46
NEVI0 <neviocostamagagnin@gmail.com>
2021-10-16 11:25:52 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+print_only_one = 1
+
+while 1:
+
+    success, img = cap.read()
+    imgRgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRgb)
+
+    if print_only_one == 1:
+        print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
