9c27f7092fc3a923dc66750e462b381912bfbebd
NEVI0 <neviocostamagagnin@gmail.com>
2021-10-16 15:14:27 -0300

    Removed print results

--- main.py
+++ main.py
@@ -6,16 +6,11 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
-print_only_one = 1
-
 while 1:
 
     success, img = cap.read()
     imgRgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRgb)
-
-    if print_only_one == 1:
-        print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
