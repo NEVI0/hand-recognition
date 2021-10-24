518e7505b4d08da8f5e670dd91112034c9625ea3
NEVI0 <neviocostamagagnin@gmail.com>
2021-10-24 09:49:39 -0300

    Added counter method and display volume bar method

--- mode_controller.py
+++ mode_controller.py
@@ -26,8 +26,29 @@ class ModeController:
         cv2.putText(img, object_text, (10, 465), FONT_FAMILY, FONT_SCALE, BLUE_COLOR, FONT_SIZE, FONT_LINE)
 
     @staticmethod
-    def use_counter_mode():
-        print('Using counter mode')
+    def use_counter_mode(img, landmark_list):
+        size = FONT_SIZE - 1
+        scale = FONT_SCALE - 0.10
+
+        if len(landmark_list[4]) > 0:
+            finger_1 = (landmark_list[4][1] - 5, landmark_list[4][2] - 15)
+            cv2.putText(img, '1', finger_1, FONT_FAMILY, scale, WHITE_COLOR, size, FONT_LINE)
+
+        if len(landmark_list[8]) > 0:
+            finger_2 = (landmark_list[8][1] - 5, landmark_list[8][2] - 15)
+            cv2.putText(img, '2', finger_2, FONT_FAMILY, scale, WHITE_COLOR, size, FONT_LINE)
+
+        if len(landmark_list[12]) > 0:
+            finger_3 = (landmark_list[12][1] - 5, landmark_list[12][2] - 15)
+            cv2.putText(img, '3', finger_3, FONT_FAMILY, scale, WHITE_COLOR, size, FONT_LINE)
+
+        if len(landmark_list[16]) > 0:
+            finger_4 = (landmark_list[16][1] - 5, landmark_list[16][2] - 15)
+            cv2.putText(img, '4', finger_4, FONT_FAMILY, scale, WHITE_COLOR, size, FONT_LINE)
+
+        if len(landmark_list[20]) > 0:
+            finger_5 = (landmark_list[20][1] - 5, landmark_list[20][2] - 15)
+            cv2.putText(img, '5', finger_5, FONT_FAMILY, scale, WHITE_COLOR, size, FONT_LINE)
 
     @staticmethod
     def use_volume_mode(img, volume_ctrl, landmark_list):
@@ -43,8 +64,9 @@ class ModeController:
         cv2.line(img, finger_1, finger_2, ORANGE_COLOR, 4)
 
         length = math.hypot(finger_2[0] - finger_1[0], finger_2[1] - finger_1[1])
-        volume_ctrl.set_volume(length)
+        volume_bar = volume_ctrl.set_volume(length)
+        volume_ctrl.display_volume_bar(img, volume_bar)
 
     @staticmethod
-    def use_object_moving_mode():
-        print('Using object moving mode')
+    def use_object_moving_mode(img):
+        cv2.rectangle(img, (250, 250), (300, 300), (255, 255, 255), -1)
